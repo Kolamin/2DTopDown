fe079ebed8e58dd3613ab18ef24622af511ecb8d
Kolamin <Levomecol@yandex.ru>
2026-02-03 16:57:00 +0600

    add progressbar

--- scene/manager/experience_manager.gd
+++ scene/manager/experience_manager.gd
@@ -1,10 +1,23 @@
 extends Node
 
+class_name ExperienceManager
+
+signal experience_update(current_experience:float, target_experience: float)
+
 var current_experience = 0
+var target_experience = 5
+var target_after_lvup = 5
+var current_level = 1
 
 func _ready() -> void:
 	Global.experince_bottle_collected.connect(on_experince_bottle_collected)
 
 func on_experince_bottle_collected(experience):
-	current_experience += experience
-	print(current_experience)
+	current_experience = min(current_experience + experience, target_experience)
+	experience_update.emit(current_experience, target_experience)
+	
+	if current_experience == target_experience:
+		current_level += 1
+		current_experience = 0
+		target_experience += target_after_lvup
+		experience_update.emit(current_experience, target_experience)
---
+++ scene/ui/experience_bar.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+@export var experience_manager: ExperienceManager
+@onready var progress_bar = $MarginContainer/ProgressBar
+
+func _ready():
+	progress_bar.value = 0
+	experience_manager.experience_update.connect(on_experiece_updated)
+	
+func on_experiece_updated(current_experience: float, target_experience: float):
+	var current_value = current_experience / target_experience
+	progress_bar.value = current_value
+	
